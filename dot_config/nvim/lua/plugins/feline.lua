ea9df62f4d110825b2d11da54dfddcf4d051685d
lyte <maxime@impalabs.com>
2024-11-09 12:46:37 +0100

    feline.nvim - file path is now relative to the cwd

--- dot_config/nvim/lua/plugins/feline.lua
+++ dot_config/nvim/lua/plugins/feline.lua
@@ -1,11 +1,20 @@
 -- A minimal, stylish and customizable statusline, statuscolumn, and winbar for Neovim
 
 return {
-	"feline-nvim/feline.nvim",
-	after = "catppuccin",
-	config = function()
-		require("feline").setup {
-			components = require("catppuccin.groups.integrations.feline").get(),
-		}
-	end,
+    "feline-nvim/feline.nvim",
+    after = "catppuccin",
+    config = function()
+        local ctp_feline = require('catppuccin.groups.integrations.feline')
+        local components = ctp_feline.get()
+        components.active[3][3].provider = function()
+            local filename = vim.fn.expand "%:."
+            local extension = vim.fn.expand "%:e"
+            local present, icons = pcall(require, "nvim-web-devicons")
+            local icon = present and icons.get_icon(filename, extension) or "󰈙"
+            return ("%m" or "") .. " " .. icon .. " " .. filename .. " "
+        end
+        require("feline").setup {
+            components = components,
+        }
+    end,
 }
