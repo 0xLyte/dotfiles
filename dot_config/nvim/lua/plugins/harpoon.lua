c70d9e46ea773a3b9c2189e9866914acb15eddf2
lyte <maxime@impalabs.com>
2025-12-15 12:04:45 +0100

    changed harpoon keybindings

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -10,16 +10,17 @@ return {
 
         keymap.set("n", "<leader>hA", function() harpoon:list():prepend() end)
         keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-
         keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        keymap.set("n", "<leader><F6>", function() harpoon:list():select(1) end)
-        keymap.set("n", "<leader><F7>", function() harpoon:list():select(2) end)
-        keymap.set("n", "<leader><F8>", function() harpoon:list():select(3) end)
-        keymap.set("n", "<leader><F9>", function() harpoon:list():select(4) end)
-        keymap.set("n", "<leader>h<F6>", function() harpoon:list():replace_at(1) end)
-        keymap.set("n", "<leader>h<F7>", function() harpoon:list():replace_at(2) end)
-        keymap.set("n", "<leader>h<F8>", function() harpoon:list():replace_at(3) end)
-        keymap.set("n", "<leader>h<F9>", function() harpoon:list():replace_at(4) end)
+        keymap.set("n", "<leader>y", function() harpoon:list():select(1) end)
+        keymap.set("n", "<leader>u", function() harpoon:list():select(2) end)
+        keymap.set("n", "<leader>i", function() harpoon:list():select(3) end)
+        keymap.set("n", "<leader>o", function() harpoon:list():select(4) end)
+        keymap.set("n", "<leader>p", function() harpoon:list():select(5) end)
+        keymap.set("n", "<leader>hy", function() harpoon:list():replace_at(1) end)
+        keymap.set("n", "<leader>hu", function() harpoon:list():replace_at(2) end)
+        keymap.set("n", "<leader>hi", function() harpoon:list():replace_at(3) end)
+        keymap.set("n", "<leader>ho", function() harpoon:list():replace_at(4) end)
+        keymap.set("n", "<leader>hp", function() harpoon:list():replace_at(4) end)
     end
 }
